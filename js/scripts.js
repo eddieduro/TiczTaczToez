85074b862d4f3a8c36d8080759c0fe361c51417c
eddie <e.jamesduro@gmail.com>
2016-01-27 19:18:02 -0800

    5th spec working

--- js/scripts.js
+++ js/scripts.js
@@ -1,12 +1,24 @@
-var Player = function(symbol){
+function Player(symbol){
 	this.symbol = symbol;
 };
-var Space = function(x, y){
+function Space(x, y){
 	this.xCoordinate = x;
     this.yCoordinate = y;
     this.player= "";
 }
 
 Space.prototype.markedBy = function(player){
-	this.player = player;
+	this.markedBy = player;
 };
+
+function Board(){
+	this.spaces = [new Space(1,1), new Space(1,2), new Space(1,3), new Space(2,1), new Space(2,2), new Space(2,3), new Space(3,1), new Space(3,2), new Space(3,3) ]
+}
+
+Board.prototype.findSpace = function(x,y) {
+  for (var index in this.spaces) {
+    if (this.spaces[index].xCoordinate === x && this.spaces[index].yCoordinate === y) {
+      return this.spaces[index];
+    }
+  }
+}
--- spec/specs.js
+++ spec/specs.js
@@ -8,24 +8,31 @@ describe("Player", function() {
 });
 
 describe('Space', function(){
-	it("returns the player's mark", function() {
+	it("returns the player's symbol", function() {
       var testSpace = new Space(1,2);
       expect(testSpace.xCoordinate).to.equal(1);
    });
 });
 
 describe('Space', function(){
-	it("returns the player's mark", function() {
+	it("returns the player's symbol", function() {
       var testSpace = new Space(1,2);
       expect(testSpace.yCoordinate).to.equal(2);
    	});
 
-	it("lets a player mark a space", function() {
+	it("lets a player mark symbol on space", function() {
       var testPlayer = new Player("X")      
       var testSpace = new Space(1,2);
       testSpace.markedBy(testPlayer)
-      expect(testSpace.player).to.equal(testPlayer);
+      expect(testSpace.markedBy).to.equal(testPlayer);
    	});
+});
+
+describe('Board', function() {
+  it("creates 9 spaces", function() {
+  	var testBoard = new Board ();
+  	expect(testBoard.spaces[1].xCoordinate).to.equal(1);
+   });
 });
 
 
