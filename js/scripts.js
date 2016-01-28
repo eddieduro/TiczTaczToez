cd7748c0992b60344152799e495fa3f8ff46bf29
eddie <e.jamesduro@gmail.com>
2016-01-27 18:47:31 -0800

    first 3 specs passing

---
+++ js/scripts.js
@@ -0,0 +1,8 @@
+var Player = function(symbol){
+	this.symbol = symbol;
+};
+var Space = function(x, y){
+	this.xCoordinate = x;
+    this.yCoordinate = y;
+}
+ 
---
+++ spec/specs.js
@@ -0,0 +1,25 @@
+describe("Player", function() {
+  describe("initialize", function() {
+    it("starts a player with a symbol of X or O", function() {
+      var testPlayer = new Player("X");
+      expect(testPlayer.symbol).to.equal("X");
+    });
+  });
+});
+
+describe('Space', function(){
+	it("returns the player's mark", function() {
+      var testSpace = new Space(1,2);
+      expect(testSpace.xCoordinate).to.equal(1);
+   });
+});
+
+describe('Space', function(){
+	it("returns the player's mark", function() {
+      var testSpace = new Space(1,2);
+      expect(testSpace.yCoordinate).to.equal(2);
+   });
+});
+
+
+
